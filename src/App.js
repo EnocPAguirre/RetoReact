522124ed057459e2d12bb13e19239e88fd7e1d87
enoc pineda <enocpinedaa@gmail.com>
2021-03-03 02:27:06 -0600

    Aprendiendo React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()}
+  }
+
+  componentDidMount(){
+    this.timeID = setInterval(
+      () => this.tick(), 1000
+    )
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timeID)
+  }
+
+  tick(){
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        Son las {this.state.date.toLocaleTimeString()}
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Clock/>
+  </div>
+
   );
 }
 
